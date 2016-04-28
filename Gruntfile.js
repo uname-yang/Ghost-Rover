deb0d05ffa9e9d4c958ff865df79181812bf739b
YuYang <god_err0r@yeah.net>
2016-04-28 11:05:00 +0800

    init

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+module.exports = function(grunt) {
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.initConfig({
+        copy: {
+          src: {
+              files: [{
+                  expand: true,
+                  cwd: 'src',
+                  src: '**',
+                  dest: 'build/'
+              }]
+          },
+            semantic: {
+                files: [{
+                    expand: true,
+                    cwd: 'semantic/dist/',
+                    src: '**',
+                    dest: 'build/assets/'
+                }]
+            },
+            font: {
+                files: [{
+                    expand: true,
+                    cwd: 'node_modules/font-awesome/fonts/',
+                    src: '**',
+                    dest: 'build/assets/fonts/'
+                }]
+            },
+            icons: {
+                files: [{
+                    expand: true,
+                    cwd: 'node_modules/octicons/octicons/',
+                    src: '*.eot',
+                    dest: 'build/assets/fonts/'
+                }, {
+                    expand: true,
+                    cwd: 'node_modules/octicons/octicons/',
+                    src: 'octicons.woff',
+                    dest: 'build/assets/fonts/'
+                }, {
+                    expand: true,
+                    cwd: 'node_modules/octicons/octicons/',
+                    src: '*.ttf',
+                    dest: 'build/assets/fonts/'
+                }]
+            }
+        },
+        clean: {
+            files: ['build/']
+        }
+    });
+
+    grunt.registerTask('default', ['copy']);
+};
